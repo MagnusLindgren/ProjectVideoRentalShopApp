b0ee7250931bd0dea0facf92c7fcabd37c16fddc
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2020-11-18 14:30:11 +0100

    Addade Onconfiguring till DbContext

--- DataBaseConnection/Context.cs
+++ DataBaseConnection/Context.cs
@@ -1,10 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataBaseConnection
 {
-    class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                //.LogTo(s => System.Diagnostics.Debug.WriteLine(s))
+                .UseLazyLoadingProxies()
+                .UseSqlServer(
+                @"server.\SQLExpress;" +
+                @"database-SaleDatabase;" +
+                @"trusted_connection=true" +
+                @"MultipleActiveResultSets=true"
+                );
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
     }
 }
