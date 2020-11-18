83bacc1f6b034f6d3e7caefd18a0165e65f7e25a
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2020-11-18 10:49:23 +0100

    Add project files.

---
+++ DataBaseConnection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBaseConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
