77c668c57528fbd1bc1a263d4209f11f1243ad92
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2020-11-20 10:50:38 +0100

    Lagt till RegisterSale() (i princip en kopia av grundkoden men modifierad för vår databas)

--- DataBaseConnection/API.cs
+++ DataBaseConnection/API.cs
@@ -13,11 +13,30 @@ namespace DataBaseConnection
             using var ctx = new Context();
             return ctx.Movies.OrderBy(m => m.Title).Skip(a).Take(b).ToList();
         }
-
-        public static Member GetMemberByFirstName(string name)
+        public static Member GetMemberByUserName(string username)
         {
             using var ctx = new Context();
-            return ctx.Members.FirstOrDefault(m => m.FirstName.ToLower() == name.ToLower());
+            return ctx.Members.FirstOrDefault(m => m.Username.ToLower() == username.ToLower());
+        }
+        public static bool RegisterSale(Member member, Movie movie)
+        {
+            using var ctx = new Context();
+            try
+            {
+                // Här säger jag åt contextet att inte oroa sig över innehållet i dessa records.
+                // Om jag inte gör detta så kommer den att försöka updatera databasens Id och cracha.
+                ctx.Entry(member).State = EntityState.Unchanged;
+                ctx.Entry(movie).State = EntityState.Unchanged;
+
+                ctx.Add(new Rental() { StartDate = DateTime.Now, RentedBy = member, Movies = movie });
+                return ctx.SaveChanges() == 1;
+            }
+            catch (DbUpdateException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                System.Diagnostics.Debug.WriteLine(e.InnerException.Message);
+                return false;
+            }
         }
     }
 }
